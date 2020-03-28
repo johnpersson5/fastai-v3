f22cae85ec6dda6c84f81d9cbc12d50cae0e681b
johnpersson5 <61058412+johnpersson5@users.noreply.github.com>
2020-03-28 00:24:35 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -13,14 +13,14 @@ from starlette.staticfiles import StaticFiles
 #path_lbl = path/'labels'
 #path_img = path/'images'
 #fnames = get_image_files(path_img)
-fnames[:3]
-lbl_names = get_image_files(path_lbl)
-lbl_names[:3]
-img_f = fnames[0]
-get_y_fn = lambda x: path_lbl/f'{x.stem}_P{x.suffix}'
-mask = open_mask(get_y_fn(img_f))
-src_size = np.array(mask.shape[1:])
-src_size,mask.data
+#fnames[:3]
+#lbl_names = get_image_files(path_lbl)
+#lbl_names[:3]
+#img_f = fnames[0]
+#get_y_fn = lambda x: path_lbl/f'{x.stem}_P{x.suffix}'
+#mask = open_mask(get_y_fn(img_f))
+#src_size = np.array(mask.shape[1:])
+#src_size,mask.data
 #codes = np.loadtxt(path/'codes.txt', dtype=str); codes
 codes = ['Animal', 'Archway', 'Bicyclist', 'Bridge', 'Building', 'Car', 'CartLuggagePram', 'Child', 'Column_Pole',
        'Fence', 'LaneMkgsDriv', 'LaneMkgsNonDriv', 'Misc_Text', 'MotorcycleScooter', 'OtherMoving', 'ParkingBlock',
