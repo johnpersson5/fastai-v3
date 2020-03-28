908b8811d267ad1b3f950db83bb504f58c09feac
johnpersson5 <61058412+johnpersson5@users.noreply.github.com>
2020-03-28 17:09:43 -0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -28,8 +28,8 @@ function analyze() {
   };
   xhr.onload = function(e) {
     if (this.readyState === 4) {
-     var response = JSON.parse(e.target.responseText);
-     el("result-label").innerHTML = `Result = ${response["result"]}`;
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
@@ -38,4 +38,3 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
