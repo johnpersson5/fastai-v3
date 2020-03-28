8057ec6b9b8bced627677ec40af448945797f384
johnpersson5 <61058412+johnpersson5@users.noreply.github.com>
2020-03-27 20:59:52 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,10 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/ep9m2xm8o92ixny/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Animal', 'Archway', 'Bicyclist', 'Bridge', 'Building', 'Car', 'CartLuggagePram', 'Child', 'Column_Pole',
+       'Fence', 'LaneMkgsDriv', 'LaneMkgsNonDriv', 'Misc_Text', 'MotorcycleScooter', 'OtherMoving', 'ParkingBlock',
+       'Pedestrian', 'Road', 'RoadShoulder', 'Sidewalk', 'SignSymbol', 'Sky', 'SUVPickupTruck', 'TrafficCone',
+       'TrafficLight', 'Train', 'Tree', 'Truck_Bus', 'Tunnel', 'VegetationMisc', 'Void', 'Wall']
 path = Path(__file__).parent
 
 app = Starlette()
